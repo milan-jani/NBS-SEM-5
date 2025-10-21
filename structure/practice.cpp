969b11ace5ade39d199bf9ef37be7c44fd5c42bb
milan-jani <janimilan7077@gmail.com>
2025-10-21 20:34:36 +0530

    practice of structure

---
+++ structure/practice.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct cartoon
+{
+    char name[20];
+    char channel[20];
+};
+
+struct cartoon2
+{
+    string name;
+    string channel;
+};
+
+void solve() {
+    // cartoon Scoobydoo;
+    // strcpy(Scoobydoo.name, "Scooby Doo");
+    // strcpy(Scoobydoo.channel, "Cartoon Network");
+    // cout << "Size of Scoobydoo: " << sizeof(Scoobydoo) << endl;
+    // cout << "Size of cartoon struct: " << sizeof(cartoon) << endl;
+
+    cartoon Tomandjerry;
+    strcpy(Tomandjerry.name, "Tom and Jerry");
+    strcpy(Tomandjerry.channel, "Cartoon Network");
+
+    cartoon2 Tomandjerry2;
+    Tomandjerry2.name = "Tom and Jerry";
+    Tomandjerry2.channel = "Cartoon Network";
+    cout << "Size of Tomandjerry2: " << sizeof(Tomandjerry2) << endl;
+    cout << "Size of cartoon2 struct: " << sizeof(cartoon2) << endl;
+
+   // cartoon* ptr = &Scoobydoo;
+}
+
+int main() {
+    solve();
+    return 0;
+}
