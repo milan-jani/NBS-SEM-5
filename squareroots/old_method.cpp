d1ffa766eaa2cc73abde17a9c00c41168c1a7e16
milan-jani <janimilan7077@gmail.com>
2025-11-13 23:59:19 +0530

    old method of finding sqrt

--- squareroots/old_method.cpp
+++ squareroots/old_method.cpp
@@ -5,25 +5,26 @@ using namespace std;
 double perdiff(double a, double b) {
     return (abs(a - b) / b) * 100.0;
 }
-double solve(int n,int s){
+double solve(double n,double s){
     double diff,avg;
     while(true){
         diff=n/s;
         avg=(s+diff)/2;
         cout<<diff<<" "<<avg<<endl;
-        double check=perdiff(s,diff);
-        cout<<check<<endl;
-        if(check<0.1) {
+       
+        //cout<<check<<endl;
+        if(perdiff(s,avg)<0.1) {
             return avg;
             break;
         }
-        n=avg;
+        s=avg;
+
     }
 }
 int main(){
-    double  n=73;
-    cout<<INT_MAX<<endl;
-    int start=1001;
+    double  n=8;
+   
+    double start=1;
     // cin>>n;
     // cin>>start;
     cout<< solve(n,start);
